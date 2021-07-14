b516e7d7e999fa2478fb95fc7f238b9c15fa8618
NWSOFT <z-c-ge@outlook.com>
2021-07-14 12:34:27 +0800

    Works? Nope.

--- main.py
+++ main.py
@@ -4,12 +4,28 @@ def count_down(seconds: int = 0):
         basic.show_number(seconds)
         basic.pause(1000)
 
+
+COMMON_VOLTAGE = 0.5
+LESSTHAN30_VOLTAGE = 1
+distance = pins.pulse_in(DigitalPin.P1, PulseValue.HIGH) / 58
 def on_forever():
-    pins.digital_write_pin(DigitalPin.P0, 1)
-    count_down(10)
-    pins.digital_write_pin(DigitalPin.P0, 0)
-    pins.digital_write_pin(DigitalPin.P1, 1)
-    count_down(10)
-    pins.digital_write_pin(DigitalPin.P1, 0)
+    global distance
+    distance = pins.pulse_in(DigitalPin.P1, PulseValue.HIGH) / 58
+    # 1 = 1 voltage
+    # 0.5 = 0.5 voltage
+    if distance > 50:
+        pins.digital_write_pin(DigitalPin.P0, 1)
+        count_down(10)
+        pins.digital_write_pin(DigitalPin.P0, 0)
+        pins.digital_write_pin(DigitalPin.P2, 1)
+        count_down(10)
+        pins.digital_write_pin(DigitalPin.P2, 0)
+    else:
+        pins.digital_write_pin(DigitalPin.P0, 0.5)
+        count_down(10)
+        pins.digital_write_pin(DigitalPin.P0, 0)
+        pins.digital_write_pin(DigitalPin.P2, 0.5)
+        count_down(10)
+        pins.digital_write_pin(DigitalPin.P2, 0)
 
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -6,11 +6,28 @@ function count_down(seconds: number = 0) {
     }
 }
 
+let COMMON_VOLTAGE = 0.5
+let LESSTHAN30_VOLTAGE = 1
+let distance = pins.pulseIn(DigitalPin.P1, PulseValue.High) / 58
 basic.forever(function on_forever() {
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    count_down(10)
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    count_down(10)
-    pins.digitalWritePin(DigitalPin.P1, 0)
+    
+    distance = pins.pulseIn(DigitalPin.P1, PulseValue.High) / 58
+    //  1 = 1 voltage
+    //  0.5 = 0.5 voltage
+    if (distance > 50) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        count_down(10)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        count_down(10)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0.5)
+        count_down(10)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0.5)
+        count_down(10)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
+    
 })
